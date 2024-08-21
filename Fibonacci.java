11e03d2a6fb363aa7b768fe4ca8b0faab4f165b1
USMAN-PRO-TECH <usmannaeem52666@gmail.com>
2024-08-21 12:45:26 +0500

    Initial commit

---
+++ Fibonacci.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of terms: ");
+        int n = sc.nextInt();
+        int first = 0, second = 1;
+
+        System.out.println("fibonacci series" + first + "," + second);
+
+        for (int i = 3; i <= n; ++i) {
+            int next = first + second;
+            System.out.print(next + ",");
+            first = second;
+            second = next;
+        }
+    }
+}
